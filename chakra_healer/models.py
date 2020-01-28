eeabed9ede15fac1b14b4828dc5e55f23701361e
Gouthum Karadi <goukaradi@hotmail.com>
2020-01-28 09:45:58 -0500

    Added chakra models and data.xslx.

--- chakra_healer/models.py
+++ chakra_healer/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Chakras(model.Model):
+    english_name = models.CharField(max_length=20)
+    sanskrit_name = models.CharField(max_length=20)
+    number = models.U=IntegerField()
+    color = models.CharField(max_length=10)
+    note = models.CharField(max_length=2)
+    base_frequency = models.IntegerField
+    octave_one = models.IntegerField()
+    octave_two = models.IntegerField()
+    description	= models.TextField()
+    sound = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.english_name
+
+class UserData(model.Model):
+    user_name = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    home_chakra_number = models.IntegerField()
+    home_note = models.CharField(max_length=2)
+    home_frequency = models.IntegerField()
+
+    def __str__(self):
+        return self.user_name
+
+class Conditions(model.Model):
+    condition = models.CharField(max_length=20)
+    primary_chakra = models.IntegerField()
+    secondary_chakra = models.IntegerField()
+    tertiary_chakra = models.IntegerField()
+    
+    def __str__(self):
+        return self.condition
